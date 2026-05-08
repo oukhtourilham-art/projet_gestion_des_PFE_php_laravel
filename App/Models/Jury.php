e06c526e9b2eb6390c39c2886b98da8b958a6e01
eurydime <nisrinehamdaoui1@gmail.com>
2026-05-08 01:10:28 +0100

    merge branch 'main' of

--- App/Models/Jury.php
+++ App/Models/Jury.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jury extends Model
 {
-    //
+    protected $fillable = [
+        'soutenance_id',
+        'professor_id',
+        'role'
+    ];
+
+    public function soutenace(){
+        return $this->belongsTo(Soutenance::class);
+    }
 }
--- App/Providers/AppServiceProvider.php
+++ App/Providers/AppServiceProvider.php
@@ -6,7 +6,9 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
+    /**use Maatwebsite\Excel\Imports\HeadingRowFormatter;
+
+HeadingRowFormatter::default('slug');
      * Register any application services.
      */
     public function register(): void
